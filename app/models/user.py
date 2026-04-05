b8f580309dbbbcf225a39c8c7a4ffede518afd0a
Pawel Borkar <pawel@duck.com>
2026-04-05 15:01:10 +0530

    fix: on_update bug and set id to be PK

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,18 @@
-import timezone
-from sqlalchemy.orm  mapped_column
+from datetime import datetime
+from sqlalchemy import DateTime, Boolean, String, func, text
+from sqlalchemy.orm import Mapped, mapped_column 
 from models.base import Base
 
 class USER(Base):
 
     __tablename__ = 'user'
 
-    id: int | None
-    username: str = mapped_column(String(16), unique=True)
-    email: str = mapped_column(unique=True)
-    password: str = mapped_column()
-    is_active: int = mapped_column(1)
+    ''' Note: Any fields with has explicit data type and does not contain | None (Optionaal None) is by default have nullable=False set by SQLAlchemy under the hood.'''
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(String(255))
+    is_active: Mapped[bool] = mapped_column(Boolean, server_default=text("true"))
 
-    created_at: DateTime = mapped_column(DateTime(timezone.utc), server_default=func.now())
-    update_at: DateTime = mapped_column(DateTime(timezone.utc), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(datetime.UTC), server_default=func.now())
+    update_at: Mapped[datetime | None] = mapped_column(DateTime(datetime.UTC), on_update=func.now(), nullable=True)
