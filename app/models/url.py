f05c1bf279465595f47e56476b3ddc0b42a29dfd
Pawel Borkar <pawel@duck.com>
2026-04-07 12:14:39 +0530

    refactor: using sqlalchemy 2.0 convention Mapped for types

--- app/models/url.py
+++ app/models/url.py
@@ -1,6 +1,6 @@
-import timezone
-from sqlalchemy import String, DateTime, Number
-from sqlalchemy.orm import mapped_column, func
+from datetime import datetime
+from sqlalchemy import String, DateTime, func 
+from sqlalchemy.orm import mapped_column, Mapped
 from models.base import Base
 
 
@@ -8,19 +8,19 @@ from models.base import Base
 class URL(Base):
     __tablename__ = "urls"
 
-    id: int | None = mapped_column(primary_key=True)
-    short_code: str = mapped_column(String(8), unique=True)
-    url: str = mapped_column(
+    id: Mapped[int] | None = mapped_column(primary_key=True)
+    short_code: Mapped[str] = mapped_column(String(8), unique=True)
+    url: Mapped[str] = mapped_column(
         String(32000)
     )  # Explicitly setting arbitary value for the destination URL in order to avoid pollution
-    # user: int: USER.id
-    count: int = mapped_column(Number(1))
-    created_at: DateTime = mapped_column(
-        DateTime(timezone.utc), server_default=func.now()
+    # user: Mapped[int]: USER.id
+    count: Mapped[int] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(datetime.UTC), server_default=func.now()
     )
-    expires_at: DateTime = mapped_column(
-        DateTime(timezone.utc), server_default=func.now()
-    )
-    updated_at: DateTime = mapped_column(
-        DateTime(timezone.utc), server_default=func.now()
+    # expires_at: Mapped[datetime] = mapped_column(
+    #     DateTime(datetime.UTC.utc), server_default=func.now()
+    # )
+    updated_at: Mapped[datetime | None] = mapped_column(
+        DateTime(datetime.UTC), server_default=func.now()
     )
