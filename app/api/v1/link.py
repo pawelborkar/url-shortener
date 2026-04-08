1a53d1faf6fa423197921c4290205e8829f2bde3
Pawel Borkar <pawel@duck.com>
2026-04-08 18:51:53 +0530

    feat: redirect to destination/original URL from short_code

--- app/api/v1/link.py
+++ app/api/v1/link.py
@@ -31,5 +31,18 @@ async def create_short_code(link: LinkCreate):
 """
 @params: Short Code
 @method: GET
-@return: Redirects to the Destination URL
+@return: Redirects to the Destination URL - uses 302 Temporary redirect
 """
+@router.get("/{short_code}")
+async def redirect_to_destination(short_code: str):
+    with shelve.open(DB_NAME) as db:
+        if short_code not in db:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                headers=None,
+                detail="URL not found.",
+            )
+
+        return RedirectResponse(
+            url=db[short_code], status_code=302, headers=None, background=None
+        )
